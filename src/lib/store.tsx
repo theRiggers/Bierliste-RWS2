e1eb8ed2c439b1c3fbec624889d062d028974ab8
Jamie Riggers <jadoherz@gmail.com>
2026-06-02 12:25:25 +0000

    https://www.paypal.me/JamieRigden932
    Das ist der richtige Account

--- src/lib/store.tsx
+++ src/lib/store.tsx
@@ -37,8 +37,8 @@ export interface Payment {
 
 export const BEER_PRICE = 1.50;
 export const CRATE_PRICE = 35.00;
-// Hier kannst du den PayPal-Link eures Vereins anpassen
-export const PAYPAL_ME_LINK = "https://paypal.me/rws2kasse";
+// Der offizielle PayPal-Link der Mannschaftskasse RWS2
+export const PAYPAL_ME_LINK = "https://www.paypal.me/JamieRigden932";
 
 interface StoreContextType {
   players: Player[];
